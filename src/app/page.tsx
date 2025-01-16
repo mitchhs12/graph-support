2c3aaf988c72dcc21bb412108dd6c9a27acb82ca
valodax <snitzel098@gmail.com>
2025-01-16 14:13:18 -0500

    :white_check_mark: `quicksave` quicksave

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
-const handleGoogleOAuth = async () => {};
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -19,13 +17,7 @@ export default function Home() {
           <li>Save and see your changes instantly.</li>
         </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <Button
-            onClick={() => {
-              handleGoogleOAuth();
-            }}
-          >
-            Sign in with Google
-          </Button>
+          <Button>Sign in with Google</Button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
--- src/auth.ts
+++ src/auth.ts
@@ -3,12 +3,4 @@ import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [Google],
-  callbacks: {
-    async signIn({ account, profile }: any) {
-      if (account.provider === "google") {
-        return profile.email_verified && profile.email.endsWith("@example.com");
-      }
-      return true; // Do different verification for other providers that don't have `email_verified`
-    },
-  },
 });
